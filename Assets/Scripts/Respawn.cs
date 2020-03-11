8f50b5c28ce3079d4d7686f0d64d607c51920b2c
CBTE Account <cbte@ac.sdmesa.net>
2020-03-11 11:44:44 -0700

    Respawning With Collision

---
+++ Assets/Scripts/Respawn.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    public Transform SpawnPoint;
+    public GameObject Player;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Player.transform.position = SpawnPoint.transform.position;
+    }
+}
