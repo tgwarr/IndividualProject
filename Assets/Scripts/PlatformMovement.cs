ffaaaaf23ee09f53d5d2b75c216bcadd035fab28
Kellen <k.willard@hotmail.com>
2020-03-27 22:51:12 -0700

    running out of time

---
+++ Assets/Scripts/PlatformMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMovement : MonoBehaviour
+{
+	public int speed;
+	public GameObject[] platforms;
+    // Start is called before the first frame update
+    void Start()
+    {
+		platforms = GameObject.FindGameObjectsWithTag("platform");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
