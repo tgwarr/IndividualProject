2c5136123e88b8b9180393ff075d366bf7494a02
Kellen <k.willard@hotmail.com>
2020-04-05 13:57:51 -0700

    Thank RNJesus I finished in time

---
+++ Assets/Scripts/DestroyObstacle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObstacle : MonoBehaviour
+{
+	private float outOfBoundsX = -100.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if(transform.position.x <= outOfBoundsX)
+		{
+			Destroy(gameObject);
+		}
+    }
+}
---
+++ Assets/Scripts/ObstacleSpawn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawn : MonoBehaviour
+{
+	private const float spawnPointX = 100;
+	private const float yRange = 10;
+	private const float spawnPointZ = 10;
+	public GameObject[] Obstacles;
+    // Start is called before the first frame update
+    void Start()
+    {
+		InvokeRepeating("SpawnObstacles", 3.0f, 1.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	void SpawnObstacles()
+	{
+		int randomSpawn = Random.Range(0, Obstacles.Length);
+		Instantiate(Obstacles[randomSpawn], new Vector3(spawnPointX, Random.Range(-yRange, yRange), spawnPointZ), Obstacles[randomSpawn].transform.rotation);
+	}
+}
